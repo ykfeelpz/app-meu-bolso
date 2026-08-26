40fc76a5d8b7953349741213a10be9fba44c91f0
Felipe <lipesbot@gmail.com>
2026-08-25 21:04:15 -0300

    Layout Correção

--- app/_layout.js
+++ app/_layout.js
@@ -2,8 +2,8 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
     return (
-        <Stack screenOptions={{ headerShown: false}}>
-            <Stack.Screen name="index" screenOptions={{ headerShown: false }} />
+        <Stack screenOptions={{ HeaderShown: false}}>
+            <Stack.Screen name="index" screenOptions={{ HclseaderShown: false }} />
             <Stack.Screen name="register" options={{ title:'Criar conta' }} />
         </Stack>
     );
